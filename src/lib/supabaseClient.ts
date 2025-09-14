78237947dc374c3d1f7ba464422ac94585565822
bryamslm <bryamlopez4@gmail.com>
2025-09-14 12:12:22 -0600

    wevc

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from '@/types-db'
 
-export const supabase = createClient<Database>(
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
